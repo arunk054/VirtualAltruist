9e7a27eb92afc4cdf66af1089befb73410aaad21
Arun Kalyanasundaram <arunkaly@cs.cmu.edu>
2014-11-11 21:28:34 -0500

    Implemented Facebook login flow.

---
+++ client/FBClient.js
@@ -0,0 +1,9 @@
+// All code here is client code
+
+		Accounts.ui.config({
+			requestPermissions: {
+			facebook: ['email', 'user_status'],
+			}
+		});
+
+
---
+++ server/accounts.js
@@ -0,0 +1,17 @@
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+    
+		Accounts.loginServiceConfiguration.remove({
+		  service: "facebook"
+		});
+		Accounts.loginServiceConfiguration.insert({
+		  service: "facebook",
+		  appId: "287242084819085",
+		  secret: "f51147ca417db876d3900da4037f4675"
+		});
+
+  });
+
+
+
