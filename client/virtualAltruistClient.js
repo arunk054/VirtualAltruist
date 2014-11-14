e03db08fa34f29ed4f9a4208594be7bae78f5c7a
Marc Estruch Tena <Marc@cmu-871868.wv.cc.cmu.edu>
2014-11-14 15:05:18 -0500

    Included UI for the profile. Right now using TODO list as tasks.
    TO TAKE IN ACCOUNT:
    - CSS file that can be used to change distribution of elements easily and properties.
    - JS pretty much the same as TODO LIST tutorial but with some minor changes to get some information
    - HTML use templates in order to create some parts.

---
+++ client/virtualAltruistClient.js
@@ -0,0 +1,79 @@
+Tasks = new Mongo.Collection("tasks");
+
+Meteor.subscribe("tasks")
+Template.body.helpers({
+  tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+  incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();    
+    },
+    totalCount: function () {
+      return Tasks.find().count();    
+    },
+    percentageDone: function () {
+      return Math.floor((Tasks.find({checked: {$ne: false}}).count()/Tasks.find().count())*100);    
+    }
+
+});
+Template.task.helpers({
+  isOwner: function () {
+    return this.owner === Meteor.userId();
+  }
+});
+
+Template.expProgressBar.helpers({
+    percentageDone: function () {
+      return Math.floor((Tasks.find({checked: {$ne: false}}).count()/Tasks.find().count())*100);    
+    },
+    totalCount: function () {
+      return Tasks.find().count();    
+    }
+});
+Template.nameAndStats.helpers({
+    getUsername: function () {
+      return Meteor.userId() && Meteor.user() ? Meteor.user().username : "No user";
+    },
+    getPointsByUser: function () {
+      return Tasks.find(
+        { checked: {$ne: false} }).count();
+    }
+});
+
+Template.body.events({
+  "submit .new-task": function(event){
+    var text = event.target.text.value;
+    Meteor.call("addTask", text);
+    event.target.text.value = "";
+
+    return false;
+  },
+  "change .hide-completed input": function (event){
+    Session.set("hideCompleted", event.target.checked);
+  }
+});
+Template.task.events({
+  "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+    Meteor.call("setChecked", this._id, ! this.checked);
+  },
+  "click .delete": function () {
+    Meteor.call("deleteTask", this._id);
+  },
+  "click .toggle-private": function () {
+    Meteor.call("setPrivate", this._id, ! this.private);
+  }
+});
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+});
+
