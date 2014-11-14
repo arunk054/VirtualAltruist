7d595aad12e34fb89a57383c7b0e4e5ff9698f41
Arun Kalyanasundaram <arunkaly@cs.cmu.edu>
2014-11-13 19:51:20 -0500

    add the FB.ui sdk to allow posting on User's wall

--- client/FBClient.js
+++ client/FBClient.js
@@ -1,9 +1,35 @@
 // All code here is client code
 
+	   
+	   window.fbAsyncInit = function(){
+		FB.init({
+		  appId      : '287242084819085',
+		  xfbml      : true,
+		  version    : 'v2.1'
+		});
+	};
+	(function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+    
 		Accounts.ui.config({
 			requestPermissions: {
-			facebook: ['email', 'user_status'],
+			facebook: ['email', 'public_profile'],
 			}
 		});
 
+	Template.body.events({
+	  "submit .new-task": function (event) {
+		FB.ui({
+		  method: 'send',
+		  link: 'http://www.nytimes.com/2011/06/15/arts/people-argue-just-to-win-scholars-assert.html',
+		});
+	  
+	  }
+	  });
 
