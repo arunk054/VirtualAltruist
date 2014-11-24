b83f56c1fd036e235d2905567c8603e8a898fbd1
Arun Kalyanasundaram <arunkaly@cs.cmu.edu>
2014-11-24 15:55:59 -0500

    Working version 1

--- client/FBClient.js
+++ client/FBClient.js
@@ -23,37 +23,9 @@
 			}
 		});
 
-	Template.body.events({
-		  "click #TwitIdButton": function (event) {
-		  	twitterId = $("#TwitterId").val();
-			//Get the Twitter Feeds
-			alert(twitterId);
-			Meteor.call('getBananaTweets',function(err,result) {
-				console.log(err);
-				console.log(result);
-			});
-	 
-// 	 var T = new Twit({
-//         consumer_key:         '1IvV7gUOvBbfEA2OsgNUX4SUp', // API key
-//         consumer_secret:      'aWXKOh8Rt0t5Z5fcV2iFdXHEFDOBM0IZ2Y8HI2annixMitPPK0', // API secret
-//         access_token:         '234259524-QqzKtRJVMaoRndFnll6evPEu5NJ0uPsi25zj1whY', 
-//         access_token_secret:  'M2KXVOvfXMPUhRnOGHCpbjr1vukdcx8WINxtZ6jd5RR7D'
-//     });
-// 
-// 			    T.get('search/tweets',
-// 				{
-// 					q: 'banana since:2011-11-11',
-// 					count: 10
-// 				},
-// 				function(err, data, response) {
-// 					consloe.log("here");
-// 					console.log(data);
-// 				}
-// 			);
-		  }
-	  });
+
 	  
-	  Template.profile_pic.helpers({
+	  Template.profilePic.helpers({
 	  	profilePic: function() {
   			id = Meteor.user().services.facebook.id;
 			return "http://graph.facebook.com/"+id+"/picture";
